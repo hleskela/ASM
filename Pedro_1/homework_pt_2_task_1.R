ac3fde377a9612ab6103673933951d4bf7c90e44
Hannes Leskelä <hleskela@kth.se>
2016-10-13 17:11:48 +0200

    Initial commit with current work

---
+++ Pedro_1/homework_pt_2_task_1.R
@@ -0,0 +1,29 @@
+library(sm)
+
+attach(aircraft)
+lgPower <- log(Power)
+lgSpan <- log(Span)
+lgLength <- log(Length)
+lgWeight <- log(Weight)
+lgSpeed <- log(Speed)
+lgRange <- log(Range)
+
+# Plots the log(weight) of an airplane as a resultant of model year, Yr
+plot(Yr,lgWeight)
+
+#Loess.smooth produces continuous values instead of the discrete ones we can use for years
+#regressionData = loess.smooth(Yr, lgWeight)
+
+# Step a)
+regressionData = supsmu(Yr,lgWeight)
+lines(regressionData, col="red")
+
+x <- unlist(regressionData[1])
+y <- unlist(regressionData[2])
+
+# Step b)
+z <- 0
+for (i in seq_along(Yr)) {
+  index = which(x == Yr[i])
+  z[i] <- log((lgWeight[i]-y[index])**2)
+}
