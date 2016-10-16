7c41b77b2efb861f1d60f235ecc182284ef91402
Hannes Leskelä <hleskela@kth.se>
2016-10-16 21:38:40 +0200

    Refine plots with bigger lines, legends and named axis

--- Pedro_1/homework_pt_2_task_1.R
+++ Pedro_1/homework_pt_2_task_1.R
@@ -16,7 +16,8 @@ plot(Yr,lgWeight, main="Plot 1", sub="The log of the weights of planes vs the ma
 
 # Step a)
 m = supsmu(Yr,lgWeight)
-lines(m, col="red")
+lines(m, col="red", lwd=2)
+legend(x=12,y=13, inset=.05, cex = 1, c("Model"), lty=c(1,1), lwd=c(2,2), col=c("red"), bg="grey96")
 
 x <- unlist(m[1])
 y <- unlist(m[2])
@@ -49,10 +50,10 @@ for (k in seq_along(Yr)) {
 }
 
 # Plotting the error squared againt the manufacturing year
-plot(Yr,e,main="Plot 2", sub="The error squared of the model vs manufacturing year")
+plot(Yr,e,ylab="e²", main="Plot 2", sub="The error squared of the model vs manufacturing year")
 # Superimposing the estimated variance
 unique_years <- unique(Yr)
-lines(unique_years,sigmaSquared, col='red')
+lines(unique_years,sigmaSquared, col='red', lwd=2)
 legend(x="topright", inset=.05, cex = 1, c("Variance"), lty=c(1,1), lwd=c(2,2), col=c("red"), bg="grey96")
 
 # Plotting the regression line with 95 % confidence interval
@@ -61,3 +62,6 @@ lines(unique_years, unlist(m[2])+(1.96*sigmaSquared),col='blue',lwd=2)
 lines(unique_years, unlist(m[2])-(1.96*sigmaSquared), col='red',lwd=2)
 lines(m, col='green',lwd=2)
 legend(x=12,y=13, inset=.05, cex = 1, c("Lower 5%","Model","Upper 5%"), lty=c(1,1), lwd=c(2,2), col=c("red","green","blue"), bg="grey96")
+
+# Hannes own test of heteroscedacity
+plot(Yr,z)
